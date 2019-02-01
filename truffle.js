d51d8aa1359b6add9257d7d426b2100418f69e2b
human <dima@ethernian.com>
2019-02-01 12:13:00 +0100

    [!] inc gas for rinkeby fix deployment

--- truffle.js
+++ truffle.js
@@ -29,7 +29,7 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: '*',
-      gas: 4500000,
+      gas: 6000000,
       gasPrice: 25000000000,
     },
     ropsten: {
@@ -48,4 +48,3 @@ module.exports = {
     },
   },
 };
-
